4c6fa18ae49d18898a4e43d2d25c328ffdfc497a
Jan Viktorin <viktorin@rehivetech.com>
2015-03-02 19:04:59 +0100

    Add basic build system

---
+++ socfpga/test.c
@@ -0,0 +1,297 @@
+/*
+    FreeRTOS V8.1.2 - Copyright (C) 2014 Real Time Engineers Ltd.
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that has become a de facto standard.             *
+     *                                                                       *
+     *    Help yourself get started quickly and support the FreeRTOS         *
+     *    project by purchasing a FreeRTOS tutorial book, reference          *
+     *    manual, or both from: http://www.FreeRTOS.org/Documentation        *
+     *                                                                       *
+     *    Thank you!                                                         *
+     *                                                                       *
+    ***************************************************************************
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+
+    >>!   NOTE: The modification to the GPL is included to allow you to     !<<
+    >>!   distribute a combined work that includes FreeRTOS without being   !<<
+    >>!   obliged to provide the source code for proprietary components     !<<
+    >>!   outside of the FreeRTOS kernel.                                   !<<
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available from the following
+    link: http://www.freertos.org/a00114.html
+
+    1 tab == 4 spaces!
+
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?"                                     *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org - Documentation, books, training, latest versions,
+    license and Real Time Engineers Ltd. contact details.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd license FreeRTOS to High
+    Integrity Systems to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/******************************************************************************
+ * This project provides two demo applications.  A simple blinky style project,
+ * and a more comprehensive test and demo application.  The
+ * mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting (defined in this file) is used to
+ * select between the two.  The simply blinky demo is implemented and described
+ * in main_blinky.c.  The more comprehensive test and demo application is
+ * implemented and described in main_full.c.
+ *
+ * This file implements the code that is not demo specific, including the
+ * hardware setup and FreeRTOS hook functions.
+ *
+ * ENSURE TO READ THE DOCUMENTATION PAGE FOR THIS PORT AND DEMO APPLICATION ON
+ * THE http://www.FreeRTOS.org WEB SITE FOR FULL INFORMATION ON USING THIS DEMO
+ * APPLICATION, AND ITS ASSOCIATE FreeRTOS ARCHITECTURE PORT!
+ *
+ */
+
+/* Standard includes. */
+#include <stdio.h>
+#include <limits.h>
+
+/* Scheduler include files. */
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
+/* Altera library includes. */
+#include "alt_timers.h"
+#include "alt_clock_manager.h"
+#include "alt_interrupt.h"
+#include "alt_globaltmr.h"
+#include "alt_address_space.h"
+#include "mmu_support.h"
+#include "cache_support.h"
+
+/*
+ * Configure the hardware as necessary to run this demo.
+ */
+static void prvSetupHardware( void );
+
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented
+within this file. */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
+/* configAPPLICATION_ALLOCATED_HEAP is set to 1 in FreeRTOSConfig.h so the
+application can define the array used as the FreeRTOS heap.  This is done so the
+heap can be forced into fast internal RAM - useful because the stacks used by
+the tasks come from this space. */
+uint8_t ucHeap[ configTOTAL_HEAP_SIZE ] __attribute__ ( ( section( ".oc_ram" ) ) );
+
+/* FreeRTOS uses its own interrupt handler code.  This code cannot use the array
+of handlers defined by the Altera drivers because the array is declared static,
+and so not accessible outside of the dirver's source file.  Instead declare an
+array for use by the FreeRTOS handler.  See:
+http://www.freertos.org/Using-FreeRTOS-on-Cortex-A-Embedded-Processors.html. */
+static INT_DISPATCH_t xISRHandlers[ ALT_INT_PROVISION_INT_COUNT ];
+
+int main( void )
+{
+	/* Configure the hardware ready to run the demo. */
+	prvSetupHardware();
+
+	printf("Started Successfully\n");
+	while(1)
+		portNOP();
+
+	/* Don't expect to reach here. */
+	return 0;
+}
+
+static void prvSetupHardware( void )
+{
+	extern uint8_t __cs3_interrupt_vector;
+	uint32_t ulSCTLR, ulVectorTable = ( uint32_t ) &__cs3_interrupt_vector;
+	const uint32_t ulVBit = 13U;
+
+	alt_int_global_init();
+	alt_int_cpu_binary_point_set( 0 );
+
+	/* Clear SCTLR.V for low vectors and map the vector table to the beginning
+	of the code. */
+	__asm( "MRC p15, 0, %0, c1, c0, 0" : "=r" ( ulSCTLR ) );
+	ulSCTLR &= ~( 1 << ulVBit );
+	__asm( "MCR p15, 0, %0, c1, c0, 0" : : "r" ( ulSCTLR ) );
+	__asm( "MCR p15, 0, %0, c12, c0, 0" : : "r" ( ulVectorTable ) );
+
+	cache_init();
+	mmu_init();
+}
+
+void vApplicationMallocFailedHook( void )
+{
+	/* Called if a call to pvPortMalloc() fails because there is insufficient
+	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+	internally by FreeRTOS API functions that create tasks, queues, software
+	timers, and semaphores.  The size of the FreeRTOS heap is set by the
+	configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+	( void ) pcTaskName;
+	( void ) pxTask;
+
+	/* Run time stack overflow checking is performed if
+	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+	function is called if a stack overflow is detected. */
+	taskDISABLE_INTERRUPTS();
+	for( ;; );
+}
+
+void vApplicationIdleHook( void )
+{
+	volatile size_t xFreeHeapSpace;
+
+	/* This is just a trivial example of an idle hook.  It is called on each
+	cycle of the idle task.  It must *NOT* attempt to block.  In this case the
+	idle task just queries the amount of FreeRTOS heap that remains.  See the
+	memory management section on the http://www.FreeRTOS.org web site for memory
+	management options.  If there is a lot of heap memory free then the
+	configTOTAL_HEAP_SIZE value in FreeRTOSConfig.h can be reduced to free up
+	RAM. */
+	xFreeHeapSpace = xPortGetFreeHeapSize();
+
+	/* Remove compiler warning about xFreeHeapSpace being set but never used. */
+	( void ) xFreeHeapSpace;
+}
+
+void vAssertCalled( const char * pcFile, unsigned long ulLine )
+{
+	volatile unsigned long ul = 0;
+
+	( void ) pcFile;
+	( void ) ulLine;
+
+	taskENTER_CRITICAL();
+	{
+		/* Set ul to a non-zero value using the debugger to step out of this
+		function. */
+		while( ul == 0 )
+		{
+			portNOP();
+		}
+	}
+	taskEXIT_CRITICAL();
+}
+
+void vApplicationTickHook( void )
+{
+}
+
+void vConfigureTickInterrupt( void )
+{
+	alt_freq_t ulTempFrequency;
+	const alt_freq_t ulMicroSecondsPerSecond = 1000000UL;
+	void FreeRTOS_Tick_Handler( void );
+
+	/* Interrupts are disabled when this function is called. */
+
+	/* Initialise the general purpose timer modules. */
+	alt_gpt_all_tmr_init();
+
+	/* ALT_CLK_MPU_PERIPH = mpu_periph_clk */
+	alt_clk_freq_get( ALT_CLK_MPU_PERIPH, &ulTempFrequency );
+
+	/* Use the local private timer. */
+	alt_gpt_counter_set( ALT_GPT_CPU_PRIVATE_TMR, ulTempFrequency / configTICK_RATE_HZ );
+
+	/* Sanity check. */
+	configASSERT( alt_gpt_time_microsecs_get( ALT_GPT_CPU_PRIVATE_TMR ) == ( ulMicroSecondsPerSecond / configTICK_RATE_HZ ) );
+
+	/* Set to periodic mode. */
+	alt_gpt_mode_set( ALT_GPT_CPU_PRIVATE_TMR, ALT_GPT_RESTART_MODE_PERIODIC );
+
+	/* The timer can be started here as interrupts are disabled. */
+	alt_gpt_tmr_start( ALT_GPT_CPU_PRIVATE_TMR );
+
+	/* Register the standard FreeRTOS Cortex-A tick handler as the timer's
+	interrupt handler.  The handler clears the interrupt using the
+	configCLEAR_TICK_INTERRUPT() macro, which is defined in FreeRTOSConfig.h. */
+	vRegisterIRQHandler( ALT_INT_INTERRUPT_PPI_TIMER_PRIVATE, ( alt_int_callback_t ) FreeRTOS_Tick_Handler, NULL );
+
+	/* This tick interrupt must run at the lowest priority. */
+	alt_int_dist_priority_set( ALT_INT_INTERRUPT_PPI_TIMER_PRIVATE, portLOWEST_USABLE_INTERRUPT_PRIORITY << portPRIORITY_SHIFT );
+
+	/* Ensure the interrupt is forwarded to the CPU. */
+	alt_int_dist_enable( ALT_INT_INTERRUPT_PPI_TIMER_PRIVATE );
+
+	/* Finally, enable the interrupt. */
+	alt_gpt_int_clear_pending( ALT_GPT_CPU_PRIVATE_TMR );
+	alt_gpt_int_enable( ALT_GPT_CPU_PRIVATE_TMR );
+
+}
+/*-----------------------------------------------------------*/
+
+void vRegisterIRQHandler( uint32_t ulID, alt_int_callback_t pxHandlerFunction, void *pvContext )
+{
+	if( ulID < ALT_INT_PROVISION_INT_COUNT )
+	{
+		xISRHandlers[ ulID ].pxISR = pxHandlerFunction;
+		xISRHandlers[ ulID ].pvContext = pvContext;
+	}
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIRQHandler( uint32_t ulICCIAR )
+{
+	uint32_t ulInterruptID;
+	void *pvContext;
+	alt_int_callback_t pxISR;
+
+	/* Re-enable interrupts. */
+	__asm ( "cpsie i" );
+
+	/* The ID of the interrupt is obtained by bitwise anding the ICCIAR value
+	with 0x3FF. */
+	ulInterruptID = ulICCIAR & 0x3FFUL;
+
+	if( ulInterruptID < ALT_INT_PROVISION_INT_COUNT )
+	{
+		/* Call the function installed in the array of installed handler
+		functions. */
+		pxISR = xISRHandlers[ ulInterruptID ].pxISR;
+		pvContext = xISRHandlers[ ulInterruptID ].pvContext;
+		pxISR( ulICCIAR, pvContext );
+	}
+}
